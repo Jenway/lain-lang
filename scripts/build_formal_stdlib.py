1d6ca133a68e46f46f0f16ef53256a9754f2d8c0
Jenway <JenwayHere@Gmail.com>
2026-09-04 01:18:15 +0800

    Move binding value lowering into formal Meta

--- scripts/build_formal_stdlib.py
+++ scripts/build_formal_stdlib.py
@@ -170,6 +170,7 @@ ABI_SUPPORT_ENTRIES = (
     "meta_copy_argument_expression",
     "meta_copy_parameter_return_expression",
     "meta_copy_two_parameter_expression",
+    "meta_copy_binding_value",
     "meta_is_import",
     "meta_import_is_std",
     "meta_import_target",
