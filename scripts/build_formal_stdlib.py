7913e92995661b4321f425e311bea3c94f2e5c91
Jenway <JenwayHere@Gmail.com>
2026-09-04 01:51:26 +0800

    Move struct field layout into formal Meta

--- scripts/build_formal_stdlib.py
+++ scripts/build_formal_stdlib.py
@@ -127,6 +127,8 @@ ABI_SUPPORT_ENTRIES = (
     "meta_find_record_binding",
     "meta_module_status",
     "meta_struct_status",
+    "meta_struct_storage_size",
+    "meta_struct_field_offset",
     "meta_find_member_return",
     "meta_is_single_decimal_argument",
     "meta_is_single_arithmetic_argument",
